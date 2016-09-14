153d2a0cb9900f3dfb0965abd7913a1f9d681f70
ryichang <ryichang@gmail.com>
2016-09-14 14:20:44 -0700

    Added link and hover styling

--- src/components/article.js
+++ src/components/article.js
@@ -5,10 +5,12 @@ export default (props) => {
   return (
         <div className="col-md-4">
           <div className="col-md-12 card">
+            <a href={props.web} target="_blank">
             <img className="card-img" src={props.image}></img>
             <p className="card-headline">{props.headline}</p>
             <p className="card-text">{props.section}</p>
             <p className="card-text">{props.snippet}</p>
+            </a>
           </div>
         </div>
   );
