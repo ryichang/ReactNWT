bdfb467689a0e804879dba6ae36a87a85ee48970
ryichang <ryichang@gmail.com>
2018-04-17 19:38:42 -0700

    Changed search query to api-key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = 'e4cbd64f281e46f882876736e874cff6';
+const API_KEY = "e4cbd64f281e46f882876736e874cff6";
 // const ROOT_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json' + API_KEY;
-const ROOT_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?appid=${API_KEY}`;
+const ROOT_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${API_KEY}`;
 const TOPIC_URL = `https://api.nytimes.com/svc/topstories/v2/`;
-const POPULAR_KEY = 'e4cbd64f281e46f882876736e874cff6';
-const POPULAR_URL = `https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=`
+const POPULAR_KEY = "e4cbd64f281e46f882876736e874cff6";
+const POPULAR_URL = `https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=`;
 
-
-export const FETCH_ARTICLE = 'FETCH_ARTICLE';
-export const FETCH_TOPIC = 'FETCH_TOPIC';
-export const SET_INPUT_CHANGE = 'SET_INPUT_CHANGE';
+export const FETCH_ARTICLE = "FETCH_ARTICLE";
+export const FETCH_TOPIC = "FETCH_TOPIC";
+export const SET_INPUT_CHANGE = "SET_INPUT_CHANGE";
 
 export function fetchArticle(article) {
   const url = `${ROOT_URL}&q=${article}`;
@@ -27,7 +26,8 @@ export function fetchArticle(article) {
 export function fetchTopic(topic) {
   let url;
   if (topic === "TopStories") {
-    url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=6c28641ec7424a5fa0ca019acd34ba24'
+    url =
+      "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=6c28641ec7424a5fa0ca019acd34ba24";
   } else if (topic === "popular") {
     url = `${POPULAR_URL}${POPULAR_KEY}`;
   } else {
@@ -35,17 +35,17 @@ export function fetchTopic(topic) {
   }
 
   const request = axios.get(url);
-  console.log('Request', request);
+  console.log("Request", request);
 
   return {
     type: FETCH_TOPIC,
     payload: request
-  }
+  };
 }
 
 export function setInputChange(term) {
   return {
     type: SET_INPUT_CHANGE,
     term: term
-  }
+  };
 }
